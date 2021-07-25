20d122ce36b590dad1082f1cde1f0acc37695a5c
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2021-07-26 00:34:47 +0530

    touch updates :pizza:

--- aie.m
+++ aie.m
@@ -1,4 +1,3 @@
-
 clc
 clear
 close all
@@ -25,3 +24,49 @@ cam = webcam;
 % Initializing the Alexnet CNN
 nnet=alexnet;
 nnet.load('alexnet_weights.mat');
+
+
+while true
+    picz=cam.snapshot;
+    %picz=imread('test.jpg');
+    picz=imresize(picz,[227,227]);
+    %picz=single(picz);
+    label=classify(nnet,picz);
+    %disp(label);
+    %if label==1
+        %disp('C');
+    %elseif label==2
+        %disp('T');
+
+    image(picz);
+    % title(num2str(label));
+    title(char(label));
+    % pause(0.1);
+    object_Name=char(label);
+
+    % if object_Name=='C'
+    %     disp('C');
+    % elseif object_Name=='T'
+    %     disp('T');
+    
+    NET.addAssembly('System.Speech');
+    % NET.addAssembly('System.IO');
+    % NET.addAssembly('System.Xml');
+    % NET.addAssembly('System.Windows.Forms');
+    % NET.addAssembly('System.Drawing');
+    % NET.addAssembly('System.Windows.Forms.DataVisualization.Charting');
+    % NET.addAssembly('System.Windows.Forms.DataVisualization.Charting.Forms');
+    % NET.addAssembly('System.Windows.Forms.DataVisualization.Charting.InteractiveChart');
+    % NET.addAssembly('System.Windows.Forms.DataVisualization.Charting.InteractiveCharts.Axes');
+    % NET.addAssembly('System.Windows.Forms.DataVisualization.Charting.InteractiveCharts.Series');
+    % NET.addAssembly('System.Windows.Forms.DataVisualization.Charting.InteractiveCharts.Tooltips');
+    % NET.addAssembly('System.Windows.Forms.DataVisualization.Charting.InteractiveCharts.Views.ScatterSeriesView');
+
+    obj=System.Speech.Synthesis.SpeechSynthesizer;
+    % obj.Speak('Test');
+
+    obj.Volume=100;
+    % obj.Rate=100;
+    Speak(obj,object_Name);  
+    drawnow; 
+end
